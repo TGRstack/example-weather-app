c85f633ee4830bd0ff366010e2ff41cdebbaaf00
Florian Mettetal <florian.mettetal@gmail.com>
2018-12-10 21:21:59 -0800

    :iphone: ($user) reopenMobile browser function

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,11 +3,13 @@ import * as React from 'react'
 
 import Routes from '../routes'
 import './global.scss'
+import { reopenAsMobileWindow } from './helpers'
 // import Helmet from './Helmet'
 // import './fonts'
 import './reset.css'
 
 export default function App() {
+  (window as any).reopenMobile = reopenAsMobileWindow // tslint:disable-line no-any
   return <Page>
     {/* <Helmet /> */}
     <Routes />
